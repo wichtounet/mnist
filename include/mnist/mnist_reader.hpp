dc2941aa71c4d0e9170ce0a0beccc74576512e64
Baptiste Wicht <baptiste.wicht@gmail.com>
2015-11-24 10:52:28 +0100

    Complete the doc

--- include/mnist/mnist_reader.hpp
+++ include/mnist/mnist_reader.hpp
@@ -21,6 +21,12 @@
 
 namespace mnist {
 
+/*!
+ * \brief Represents a complete mnist dataset
+ * \tparam Container The container to use
+ * \tparam Image The type of image
+ * \tparam Label The type of label
+ */
 template<template<typename...> class Container, typename Image, typename Label>
 struct MNIST_dataset {
     Container<Image> training_images;
@@ -28,6 +34,13 @@ struct MNIST_dataset {
     Container<Label> training_labels;
     Container<Label> test_labels;
 
+    /*!
+     * \brief Resize the training set to new_size
+     *
+     * If new_size is less than the current size, this function has no effect.
+     *
+     * \param new_size The size to resize the training sets to.
+     */
     void resize_training(std::size_t new_size){
         if(training_images.size() > new_size){
             training_images.resize(new_size);
@@ -35,6 +48,13 @@ struct MNIST_dataset {
         }
     }
 
+    /*!
+     * \brief Resize the test set to new_size
+     *
+     * If new_size is less than the current size, this function has no effect.
+     *
+     * \param new_size The size to resize the test sets to.
+     */
     void resize_test(std::size_t new_size){
         if(test_images.size() > new_size){
             test_images.resize(new_size);
@@ -43,6 +63,12 @@ struct MNIST_dataset {
     }
 };
 
+/*!
+ * \brief Extract the MNIST header from the given buffer
+ * \param buffer The current buffer
+ * \param position The current reading positoin
+ * \return The value of the mnist header
+ */
 inline uint32_t read_header(const std::unique_ptr<char[]>& buffer, size_t position){
     auto header = reinterpret_cast<uint32_t*>(buffer.get());
 
@@ -50,6 +76,13 @@ inline uint32_t read_header(const std::unique_ptr<char[]>& buffer, size_t positi
     return (value << 24) | ((value << 8) & 0x00FF0000) | ((value >> 8) & 0X0000FF00) | (value >> 24);
 }
 
+/*!
+ * \brief Read a MNIST image file inside the given container
+ * \param images The container to fill with the images
+ * \param path The path to the image file
+ * \param limit The maximum number of elements to read (0: no limit)
+ * \param func The functor to create the image object
+ */
 template<template<typename...> class Container = std::vector, typename Image, typename Functor>
 void read_mnist_image_file(Container<Image>& images, const std::string& path, std::size_t limit, Functor func){
     std::ifstream file;
@@ -102,6 +135,12 @@ void read_mnist_image_file(Container<Image>& images, const std::string& path, st
     }
 }
 
+/*!
+ * \brief Read a MNIST label file inside the given container
+ * \param images The container to fill with the labels
+ * \param path The path to the label file
+ * \param limit The maximum number of elements to read (0: no limit)
+ */
 template<template<typename...> class  Container = std::vector, typename Label = uint8_t>
 void read_mnist_label_file(Container<Label>& labels, const std::string& path, std::size_t limit = 0){
     std::ifstream file;
@@ -148,6 +187,15 @@ void read_mnist_label_file(Container<Label>& labels, const std::string& path, st
     }
 }
 
+/*!
+ * \brief Read all training images and return a container filled with the images.
+ *
+ * The dataset is assumed to be in a mnist subfolder
+ *
+ * \param limit The maximum number of elements to read (0: no limit)
+ * \param func The functor to create the image objects.
+ * \return Container filled with the images
+ */
 template<template<typename...> class Container = std::vector, typename Image, typename Functor>
 Container<Image> read_training_images(std::size_t limit, Functor func){
     Container<Image> images;
@@ -155,6 +203,15 @@ Container<Image> read_training_images(std::size_t limit, Functor func){
     return images;
 }
 
+/*!
+ * \brief Read all test images and return a container filled with the images.
+ *
+ * The dataset is assumed to be in a mnist subfolder
+ *
+ * \param limit The maximum number of elements to read (0: no limit)
+ * \param func The functor to create the image objects.
+ * \return Container filled with the images
+ */
 template<template<typename...> class Container = std::vector, typename Image, typename Functor>
 Container<Image> read_test_images(std::size_t limit, Functor func){
     Container<Image> images;
@@ -162,6 +219,14 @@ Container<Image> read_test_images(std::size_t limit, Functor func){
     return images;
 }
 
+/*!
+ * \brief Read all training label and return a container filled with the labels.
+ *
+ * The dataset is assumed to be in a mnist subfolder
+ *
+ * \param limit The maximum number of elements to read (0: no limit)
+ * \return Container filled with the labels
+ */
 template<template<typename...> class Container = std::vector, typename Label = uint8_t>
 Container<Label> read_training_labels(std::size_t limit){
     Container<Label> labels;
@@ -169,6 +234,14 @@ Container<Label> read_training_labels(std::size_t limit){
     return labels;
 }
 
+/*!
+ * \brief Read all test label and return a container filled with the labels.
+ *
+ * The dataset is assumed to be in a mnist subfolder
+ *
+ * \param limit The maximum number of elements to read (0: no limit)
+ * \return Container filled with the labels
+ */
 template<template<typename...> class Container = std::vector, typename Label = uint8_t>
 Container<Label> read_test_labels(std::size_t limit){
     Container<Label> labels;
@@ -176,6 +249,15 @@ Container<Label> read_test_labels(std::size_t limit){
     return labels;
 }
 
+/*!
+ * \brief Read dataset and assume images in 3D (1x28x28)
+ *
+ * The dataset is assumed to be in a mnist subfolder
+ *
+ * \param training_limit The maximum number of elements to read from training set (0: no limit)
+ * \param test_limit The maximum number of elements to read from test set (0: no limit)
+ * \return The dataset
+ */
 template<template<typename...> class Container, typename Image, typename Label = uint8_t>
 MNIST_dataset<Container, Image, Label> read_dataset_3d(std::size_t training_limit = 0, std::size_t test_limit = 0){
     MNIST_dataset<Container, Image, Label> dataset;
@@ -189,6 +271,15 @@ MNIST_dataset<Container, Image, Label> read_dataset_3d(std::size_t training_limi
     return dataset;
 }
 
+/*!
+ * \brief Read dataset.
+ *
+ * The dataset is assumed to be in a mnist subfolder
+ *
+ * \param training_limit The maximum number of elements to read from training set (0: no limit)
+ * \param test_limit The maximum number of elements to read from test set (0: no limit)
+ * \return The dataset
+ */
 template<template<typename...> class Container, typename Image, typename Label = uint8_t>
 MNIST_dataset<Container, Image, Label> read_dataset_direct(std::size_t training_limit = 0, std::size_t test_limit = 0){
     MNIST_dataset<Container, Image, Label> dataset;
