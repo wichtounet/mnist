fb3f8ee6129e0b06eaeaecb97fb2398b63ef7b17
Eidelen Yoga <adrian.schneider@unibas.ch>
2017-07-31 23:31:35 +0200

    Add cmake example

---
+++ example/main.cpp
@@ -0,0 +1,42 @@
+/****************************************************************************
+** Copyright (c) 2017 Adrian Schneider
+**
+** Permission is hereby granted, free of charge, to any person obtaining a
+** copy of this software and associated documentation files (the "Software"),
+** to deal in the Software without restriction, including without limitation
+** the rights to use, copy, modify, merge, publish, distribute, sublicense,
+** and/or sell copies of the Software, and to permit persons to whom the
+** Software is furnished to do so, subject to the following conditions:
+**
+** The above copyright notice and this permission notice shall be included in
+** all copies or substantial portions of the Software.
+**
+** THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+** IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+** FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+** AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+** LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+** FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+** DEALINGS IN THE SOFTWARE.
+**
+*****************************************************************************/
+
+#include <iostream>
+#include "mnist/mnist_reader.hpp"
+
+int main(int argc, char* argv[])
+{
+    // MNIST_DATA_LOCATION set by MNIST cmake config
+    std::cout << "MNIST data directory: " << MNIST_DATA_LOCATION << std::endl;
+
+    // Load MNIST data
+    mnist::MNIST_dataset<std::vector, std::vector<uint8_t>, uint8_t> dataSet =
+            mnist::read_dataset<std::vector, std::vector, uint8_t, uint8_t>(MNIST_DATA_LOCATION);
+
+    std::cout << "Nbr of training images = " << dataSet.training_images.size() << std::endl;
+    std::cout << "Nbr of training labels = " << dataSet.training_labels.size() << std::endl;
+    std::cout << "Nbr of test images = " << dataSet.test_images.size() << std::endl;
+    std::cout << "Nbr of test labels = " << dataSet.test_labels.size() << std::endl;
+
+    return 0;
+}
