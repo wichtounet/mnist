630c20bab23b9eda864f4f94c5e92ffd99b86e0e
Eidelen Yoga <adrian.schneider@unibas.ch>
2017-07-31 23:28:57 +0200

    Add cmake config

---
+++ MNISTConfig.cmake
@@ -0,0 +1,7 @@
+# Config file for the MNIST package
+# It defines the following variables
+#  MNIST_INCLUDE_DIR   - include directory of MNIST
+#  MNIST_DATA_DIR      - directory of the actual MNIST data
+
+set(MNIST_INCLUDE_DIR ${CMAKE_CURRENT_LIST_DIR}/include)
+set(MNIST_DATA_DIR ${CMAKE_CURRENT_LIST_DIR})
