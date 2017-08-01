2c59d027071fe37fb80055b1833993598c3f97ef
Eidelen Yoga <adrian.schneider@unibas.ch>
2017-08-01 21:29:10 +0200

    Add MNIST_FOUND variable to cmake config

--- MNISTConfig.cmake
+++ MNISTConfig.cmake
@@ -2,6 +2,8 @@
 # It defines the following variables
 #  MNIST_INCLUDE_DIR   - include directory of MNIST
 #  MNIST_DATA_DIR      - directory of the actual MNIST data
+#  MNIST_FOUND         - MNIST is available
 
 set(MNIST_INCLUDE_DIR ${CMAKE_CURRENT_LIST_DIR}/include)
 set(MNIST_DATA_DIR ${CMAKE_CURRENT_LIST_DIR})
+set(MNIST_FOUND TRUE)
